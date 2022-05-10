021587b242a529e77360c5875cc16e05c483ef26
Ahmed HM <ahmedhadjmohammed@outlook.com>
2022-05-10 12:05:07 +0100

    Implement Service layer

---
+++ Kata/Kata.Model/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Kata.Model
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+    }
+}
---
+++ Kata/Kata.Services/GildedRoseService.cs
@@ -0,0 +1,19 @@
+﻿using Kata.Domain.Helpers;
+using Kata.Model;
+using System.Collections.Generic;
+
+namespace Kata.Services
+{
+    public class GildedRoseService
+    {       
+        public void UpdateItems(IList<Item> items)
+        {
+            foreach (var item in items)
+            {
+                var manager = ItemManagerFactory.Create(item.Name);
+                manager.Update(item);              
+            }
+        }
+
+    }
+}
